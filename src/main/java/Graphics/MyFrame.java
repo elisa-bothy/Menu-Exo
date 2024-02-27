d9654f94a491f2e37061aff2678cb2edaa5bd7d3
Elisa Bothy <elisa.bothy@gmail.com>
2024-02-27 09:50:11 +0100

    Version initiale, avant correction, exo fini

---
+++ src/main/java/Graphics/MyFrame.java
@@ -0,0 +1,125 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Graphics;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+/**
+ *
+ * @author Elisa Bothy
+ */
+public class MyFrame extends JFrame implements ActionListener{
+
+    private JMenuBar jmb;
+    private JMenuItem red, green, blue, quit;
+    private JMenu color, file;
+    private JPanel jp;
+
+    private static final long serialVersionUID = 1L;
+
+    public MyFrame(String title) throws HeadlessException {
+        super(title);
+        jmb = new JMenuBar();
+        file = new JMenu("File");
+        color = new JMenu("Color");
+        blue = new JMenuItem("Blue");
+        green = new JMenuItem("Green");
+        red = new JMenuItem("Red");
+        quit = new JMenuItem("Quit");
+        jp = new JPanel();
+        
+
+        initGui();
+        initEvents();
+
+        this.pack();
+        //centrer fenêtre
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
+    }
+
+    private void initGui() {
+        file.setMnemonic(KeyEvent.VK_F);
+        red.setMnemonic(KeyEvent.VK_R);
+        green.setMnemonic(KeyEvent.VK_G);
+        blue.setMnemonic(KeyEvent.VK_B);
+        color.setMnemonic(KeyEvent.VK_C);
+        quit.setMnemonic(KeyEvent.VK_Q);
+        red.setAccelerator(
+                KeyStroke.getKeyStroke(
+                        KeyEvent.VK_R, 
+                        InputEvent.CTRL_DOWN_MASK
+                )
+        );
+        green.setAccelerator(
+                KeyStroke.getKeyStroke(
+                        KeyEvent.VK_G, 
+                        InputEvent.CTRL_DOWN_MASK
+                )
+        );
+        blue.setAccelerator(
+                KeyStroke.getKeyStroke(
+                        KeyEvent.VK_B, 
+                        InputEvent.CTRL_DOWN_MASK
+                )
+        );
+        quit.setAccelerator(
+                KeyStroke.getKeyStroke(
+                        KeyEvent.VK_Q, 
+                        InputEvent.CTRL_DOWN_MASK
+                )
+        );
+        file.add(quit);
+        file.add(quit);
+        color.add(blue);
+        color.add(green);
+        color.add(red);
+        jmb.add(file);
+        jmb.add(color);
+        
+        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+        jp.setPreferredSize(new Dimension(200, 400));
+        jp.setOpaque(true);
+        this.add(jp);
+        this.setJMenuBar(jmb);
+    }
+
+    private void initEvents() {
+        red.addActionListener(this);
+        green.addActionListener(this);
+        blue.addActionListener(this);
+        quit.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if (e.getActionCommand().equals("Red")){
+            jp.setBackground(Color.RED);
+        }
+        if (e.getActionCommand().equals("Blue")){
+            jp.setBackground(Color.BLUE);
+        }
+        if (e.getActionCommand().equals("Green")){
+            jp.setBackground(Color.GREEN);
+        }
+        if (e.getActionCommand().equals("Quit")){
+            System.exit(0);
+        }
+    }
+}
---
+++ src/main/java/fr/ldnr/elisa/menuexo/MenuExo.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package fr.ldnr.elisa.menuexo;
+
+import Graphics.MyFrame;
+
+/**
+ *
+ * @author Elisa Bothy
+ */
+public class MenuExo {
+
+    public static void main(String[] args) {
+        MyFrame mf = new MyFrame("Menu Couleur");
+    }
+}
